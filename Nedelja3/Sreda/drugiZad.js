4636278ba943c74317356a61989e22e764239e95
Marija Bozic <BozicMJS8@gmail.com>
2020-07-29 20:53:29 +0200

    koment...

--- Nedelja3/Sreda/drugiZad.js
+++ Nedelja3/Sreda/drugiZad.js
@@ -7,7 +7,7 @@ let pP, cQcm
 function cmQ(r, cena) {
     if(r > 0 && cena > 0) {
         pP = r * r * Math.PI 
-        console.log(pP)     //(pP.toFixed(2))
+        // console.log(pP)     //(pP.toFixed(2))
         cQcm = cena / pP
         console.log(cQcm)   // (cQcm.toFixed(2))
         }
@@ -16,4 +16,6 @@ function cmQ(r, cena) {
         }
     }
 
-// cmQ(18, 1498)
+// cmQ(21, 1499)
+// cmQ(18, 1458)
+// cmQ(25, 1970)
