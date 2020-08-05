467ef4c2868d8b08b8da2fd05bd85579bb2222ef
Marija Bozic <marijabozic@outlook.com>
2020-08-05 13:44:48 +0200

    izmena prvog

--- Nedelja4/Sreda/sredaZad1.js
+++ Nedelja4/Sreda/sredaZad1.js
@@ -7,3 +7,37 @@
   
 Dalje, napraviti niz od ovih objekata. */
 
+let Pok1 = {
+  ime : 'Primarina',
+  vrsta : ['Water', 'Fairy'],
+  sposobnosti : ['(hidden) Torrent Liquid Voice'],
+  karakteristike : {Att : 74, Def : 74, Spd : 60},
+}
+// console.log(Pok1)
+
+let Pok2 = {
+  ime : 'Toucannon',
+  vrsta : ['Normal', 'Flying'],
+  sposobnosti : ['Keen Eye', 'Skill Link', '(hidden) Sheer Force'],
+  karakteristike : {Att : 120, Def : 75, Spd : 60},
+} 
+// console.log(Pok2)
+
+let Pok3 = {
+  ime : 'Mudbray',
+  vrsta : 'Ground',
+  sposobnosti : ['Own Tempo', 'Stamina', '(hidden) Inner Focus'],
+  karakteristike : {Att : 100, Def : 70, Spd : 45},
+}
+// console.log(Pok3)
+
+let Pok4 = {
+  ime : 'Shiinotic',
+  vrsta : ['Grass', 'Fairy'],
+  sposobnosti : ['Illuminaate', 'Effect Spore', 'Rain Dish'],
+  karakteristike : {Att : 45, Def : 80, Spd : 30}
+}
+// console.log(Pok4)
+
+let nizPok = [Pok1, Pok2, Pok3, Pok4]
+  console.log(nizPok) 
