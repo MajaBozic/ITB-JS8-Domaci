3c8c8a09fa8a29f14dbdcf435a051818818265a2
Marija Bozic <marijabozic@outlook.com>
2020-08-08 20:59:46 +0200

    niz sposobnosti samo od stringova

--- Nedelja4/Sreda/sredaZadaci.js
+++ Nedelja4/Sreda/sredaZadaci.js
@@ -10,43 +10,43 @@ console.log('Zadatak1.js')
 Dalje, napraviti niz od ovih objekata. */
 
 let Pok1 = {
-  ime: 'Primarina',
-  vrsta: ['Water', 'Fairy'],
-  sposobnosti: ['(hidden) Torrent Liquid Voice'],
-  karakteristike: { Nap: 74, Odbr: 74, Brzn: 60 },
+  ime : 'Primarina',
+  vrsta : ['Water', 'Fairy'],
+  sposobnosti : ['(hidden) Torrent Liquid Voice'],
+  karakteristike : {Nap : 74, Odbr : 74, Brzn : 60},
 }
 // console.log(Pok1)
 
 let Pok2 = {
-  ime: 'Toucannon',
-  vrsta: ['Normal', 'Flying'],
-  sposobnosti: ['Keen Eye', 'Skill Link', '(hidden) Sheer Force'],
-  karakteristike: { Nap: 120, Odbr: 75, Brzn: 60 },
-}
+  ime : 'Toucannon',
+  vrsta : ['Normal', 'Flying'],
+  sposobnosti : ['Keen Eye', 'Skill Link', '(hidden) Sheer Force'],
+  karakteristike : {Nap : 120, Odbr : 75, Brzn : 60},
+} 
 // console.log(Pok2)
 
 let Pok3 = {
-  ime: 'Mudbray',
-  vrsta: 'Ground',
-  sposobnosti: ['Own Tempo', 'Stamina', '(hidden) Inner Focus'],
-  karakteristike: { Nap: 100, Odbr: 70, Brzn: 45 },
+  ime : 'Mudbray',
+  vrsta : 'Ground',
+  sposobnosti : ['Own Tempo', 'Stamina', '(hidden) Inner Focus'],
+  karakteristike : {Nap : 100, Odbr : 70, Brzn : 45},
 }
 // console.log(Pok3)
 
 let Pok4 = {
-  ime: 'Shiinotic',
-  vrsta: ['Grass', 'Fairy'],
-  sposobnosti: ['Illuminaate', 'Effect Spore', 'Rain Dish'],
-  karakteristike: { Nap: 45, Odbr: 80, Brzn: 30 }
+  ime : 'Shiinotic',
+  vrsta : ['Grass', 'Fairy'],
+  sposobnosti : ['Illuminaate', 'Effect Spore', 'Rain Dish'],
+  karakteristike : {Nap : 45, Odbr : 80, Brzn : 30}
 }
 // console.log(Pok4)
 
 let nizPok = [Pok1, Pok2, Pok3, Pok4]
-console.log(nizPok)
-console.log('************************************************************')
+  console.log(nizPok) 
+  console.log('************************************************************')
 
 
-console.log('Zadatak2.js')
+  console.log('Zadatak2.js')
 
 /* Napraviti funkciju koja prima niz gore napravljenih objekata i vraca jedan niz sposobnosti svih pokemona 
  ```
@@ -54,6 +54,7 @@ console.log('Zadatak2.js')
  ```
 */
 
+nizPok = [Pok1, Pok2, Pok3, Pok4]
 
 function nizSposP(nizPok) {
   let nizSpos = []
@@ -62,6 +63,8 @@ function nizSposP(nizPok) {
   })
   return nizSpos.flat()
 }
+
+console.log(nizSposP(nizPok));
 
 
 console.log('************************************************************')
@@ -73,7 +76,7 @@ console.log('Zadatak3.js')
 
 nizPok = [Pok1, Pok2, Pok3, Pok4]
 
-nizPok.sort((a, b) => a.karakteristike.Brzn - b.karakteristike.Brzn)
+nizPok.sort((a,b) => a.karakteristike.Brzn - b.karakteristike.Brzn) 
 
 console.log(nizPok)
 
